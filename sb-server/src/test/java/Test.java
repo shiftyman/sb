fa63555a1a13ae7474eaabd7c5a9dfb8bc904401
Shiftyman <1034853321@qq.com>
2016-03-04 19:30:45 +0800

    test

--- sb-server/src/test/java/Test.java
+++ sb-server/src/test/java/Test.java
@@ -1,9 +1,10 @@
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
 
 public class Test {
     public static void main(String[] args){
-        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3);  
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(3); 
     }
 }
